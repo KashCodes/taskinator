ba7048d30c39f60f28c7b34004827d9dfbdb78f3
Kash <kashay@agyeman.me>
2021-01-01 14:19:21 -0700

    dropzone highlights only when hovered - fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -199,7 +199,7 @@ var dropZoneDragHandler = function(event) {
   var taskListEl = event.target.closest(".task-list");
   if (taskListEl) {
     event.preventDefault();
-    
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
   }
 };
 
@@ -224,8 +224,17 @@ var dropTaskHandler = function(event) {
     statusSelectEl.selectedIndex = 2;
   }
 
+  dropZoneEl.removeAttribute("style");
+
   dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    taskListEl.removeAttribute("style");
+  }
+}
 
 
 // for dragging tasks
@@ -236,6 +245,9 @@ pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 // for dropping the taks 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+// for dropzone highlight to revert back when not hovered
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
